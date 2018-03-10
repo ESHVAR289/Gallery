bd965d271a18c7790067518810126b95e3f54b77
ESHVAR MALI <eshvar89@hotmail.com>
2018-03-10 19:33:14 +0530

    Initialize the views of GalleryActivity and populate the GridView with the gallery images.

--- src/main/java/com/einfoplanet/gallery/GalleryActivity.java
+++ src/main/java/com/einfoplanet/gallery/GalleryActivity.java
@@ -1,13 +1,97 @@
 package com.einfoplanet.gallery;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridView;
 
-public class GalleryActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class GalleryActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private static Button selectImages;
+    private static GridView galleryImagesGridView;
+    private static ArrayList<String> galleryImageUrls;
+    private static GridAdapter imagesAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
+        initViews();
+        setListeners();
+        fetchGalleryImages();
+        setUpGridView();
+    }
+
+    //Init all views
+    private void initViews() {
+        selectImages = (Button) findViewById(R.id.btn_return);
+        galleryImagesGridView = (GridView) findViewById(R.id.galleryImagesGridView);
+    }
+
+    //fetch all images from gallery
+    private void fetchGalleryImages() {
+        final String[] columns = {MediaStore.Images.Media.DATA, MediaStore.Images.Media._ID};//get all columns of type images
+        final String orderBy = MediaStore.Images.Media.DATE_TAKEN;//order data by date
+        Cursor imagecursor = managedQuery(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, columns, null,
+                null, orderBy + " DESC");//get all data in Cursor by sorting in DESC order
+
+        galleryImageUrls = new ArrayList<String>();//Init array
+
+        //Loop to cursor count
+        for (int i = 0; i < imagecursor.getCount(); i++) {
+            imagecursor.moveToPosition(i);
+            int dataColumnIndex = imagecursor.getColumnIndex(MediaStore.Images.Media.DATA);//get column index
+            galleryImageUrls.add(imagecursor.getString(dataColumnIndex));//get Image from column index
+            System.out.println("Array path" + galleryImageUrls.get(i));
+        }
+    }
+
+    //Set Up GridView method
+    private void setUpGridView() {
+        imagesAdapter = new GridAdapter(GalleryActivity.this, galleryImageUrls, true);
+        galleryImagesGridView.setAdapter(imagesAdapter);
+    }
+
+    //Set Listeners method
+    private void setListeners() {
+        selectImages.setOnClickListener(this);
+    }
+
+
+    //Show hide select button if images are selected or deselected
+    public void showSelectButton() {
+        ArrayList<String> selectedItems = imagesAdapter.getCheckedItems();
+        if (selectedItems.size() > 0) {
+            selectImages.setText(selectedItems.size() + " - Images Selected");
+            selectImages.setVisibility(View.VISIBLE);
+        } else
+            selectImages.setVisibility(View.GONE);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_return:
+
+                //When button is clicked then fill array with selected images
+                ArrayList<String> selectedItems = imagesAdapter.getCheckedItems();
+
+                //Send back result to MainActivity with selected images
+                Intent intent = new Intent();
+//                intent.putExtra(LauncherActivity.CustomGalleryIntentKey, selectedItems.toString());//Convert Array into string to pass data
+                setResult(RESULT_OK, intent);//Set result OK
+                finish();//finish activity
+                break;
+
+        }
+
     }
 }
