fd2efdfae1b65398918867851308c38f4fa51439
ESHVAR MALI <eshvar89@hotmail.com>
2018-03-10 21:58:04 +0530

    Click on the img set the checked icon.

--- src/main/java/com/einfoplanet/gallery/GridAdapter.java
+++ src/main/java/com/einfoplanet/gallery/GridAdapter.java
@@ -10,6 +10,8 @@ import android.widget.BaseAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
@@ -66,12 +68,14 @@ public class GridAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View view, ViewGroup viewGroup) {
+    public View getView(final int position, View view, ViewGroup viewGroup) {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         if (view == null)
             view = inflater.inflate(R.layout.custom_grid_view_single_item, viewGroup, false);//Inflate layout
 
-        CheckBox mCheckBox = (CheckBox) view.findViewById(R.id.selectCheckBox);
+        final RelativeLayout rlContainer = view.findViewById(R.id.rl_container);
+        final LinearLayout llImgSelectContainer = view.findViewById(R.id.ll_img_select_true);
+        final CheckBox mCheckBox = (CheckBox) view.findViewById(R.id.selectCheckBox);
         final ImageView imageView = (ImageView) view.findViewById(R.id.galleryImageView);
 
         //If Context is MainActivity then hide checkbox
@@ -80,9 +84,29 @@ public class GridAdapter extends BaseAdapter {
 
         ImageLoader.getInstance().displayImage("file://" + imageUrls.get(position), imageView, options);//Load Images over ImageView
 
+        if (isCustomGalleryActivity) {
+            rlContainer.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mCheckBox.performClick();
+                }
+            });
+        }
         mCheckBox.setTag(position);//Set Tag for CheckBox
         mCheckBox.setChecked(mSparseBooleanArray.get(position));
-        mCheckBox.setOnCheckedChangeListener(mCheckedChangeListener);
+        mCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mSparseBooleanArray.put((Integer) buttonView.getTag(), isChecked);//Insert selected checkbox value inside boolean array
+                ((GalleryActivity) context).showSelectButton();//call custom gallery activity method
+                if (isChecked) {
+                    llImgSelectContainer.setVisibility(View.VISIBLE);
+                }else {
+                    llImgSelectContainer.setVisibility(View.GONE);
+                }
+            }
+        });
+//        mCheckBox.setOnCheckedChangeListener(mCheckedChangeListener);
         return view;
     }
 
