0495083193a8a7be22d29a1dbb622bd9854cd127
ESHVAR MALI <eshvar89@hotmail.com>
2018-03-10 18:39:48 +0530

    Design the basic launcher layout.

--- src/main/java/com/einfoplanet/gallery/LauncherActivity.java
+++ src/main/java/com/einfoplanet/gallery/LauncherActivity.java
@@ -1,13 +1,20 @@
 package com.einfoplanet.gallery;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 public class LauncherActivity extends AppCompatActivity {
 
+    private RecyclerView selectedImgRecyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        selectedImgRecyclerView = findViewById(R.id.selected_img_recycler_view);
+    }
+
+    public void openGalleryActivity(View view) {
     }
 }
