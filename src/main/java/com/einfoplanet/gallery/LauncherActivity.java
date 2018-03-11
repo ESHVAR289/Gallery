8d2aee222e2bba6a7106b3fb77af20d508361513
ESHVAR MALI <eshvar89@hotmail.com>
2018-03-11 20:15:12 +0530

    Added the required code to ask the permission for read externala storage.

--- src/main/java/com/einfoplanet/gallery/LauncherActivity.java
+++ src/main/java/com/einfoplanet/gallery/LauncherActivity.java
@@ -1,7 +1,17 @@
 package com.einfoplanet.gallery;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatButton;
 import android.util.SparseBooleanArray;
@@ -19,6 +29,7 @@ public class LauncherActivity extends AppCompatActivity implements View.OnClickL
     public static final String KEY_IMAGE_DATA_FROM_LAUNCHER_SCREEN = "launcher_screen_data";
     private ArrayList<ImgDetailDO> selectedImagesToSendOnGridActivity;
     private TextView txtNoData;
+    private static final int SEND_SMS_FROM_SELECTED_SIM_CARD = 102;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,10 +59,30 @@ public class LauncherActivity extends AppCompatActivity implements View.OnClickL
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.openCustomGallery:
-                //Start Custom Gallery Activity by passing intent id
-                Intent gridActivityIntent = new Intent(LauncherActivity.this, GalleryActivity.class);
-                gridActivityIntent.putExtra(KEY_IMAGE_DATA_FROM_LAUNCHER_SCREEN, selectedImagesToSendOnGridActivity);//Convert Array into string to pass data
-                startActivityForResult(gridActivityIntent, CustomGallerySelectId);
+
+                if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1) {
+
+                    if (ContextCompat.checkSelfPermission(LauncherActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                            != PackageManager.PERMISSION_GRANTED) {
+                        if (ActivityCompat.shouldShowRequestPermissionRationale(LauncherActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                            buildAlertMessageForPermission();
+                        } else {
+                            ActivityCompat.requestPermissions(
+                                    LauncherActivity.this, new String[]{
+                                            Manifest.permission.READ_EXTERNAL_STORAGE}, SEND_SMS_FROM_SELECTED_SIM_CARD);
+                        }
+                    } else {
+                        //Start Custom Gallery Activity by passing intent id
+                        Intent gridActivityIntent = new Intent(LauncherActivity.this, GalleryActivity.class);
+                        gridActivityIntent.putExtra(KEY_IMAGE_DATA_FROM_LAUNCHER_SCREEN, selectedImagesToSendOnGridActivity);//Convert Array into string to pass data
+                        startActivityForResult(gridActivityIntent, CustomGallerySelectId);
+                    }
+                } else {
+                    //Start Custom Gallery Activity by passing intent id
+                    Intent gridActivityIntent = new Intent(LauncherActivity.this, GalleryActivity.class);
+                    gridActivityIntent.putExtra(KEY_IMAGE_DATA_FROM_LAUNCHER_SCREEN, selectedImagesToSendOnGridActivity);//Convert Array into string to pass data
+                    startActivityForResult(gridActivityIntent, CustomGallerySelectId);
+                }
                 break;
         }
     }
@@ -78,9 +109,85 @@ public class LauncherActivity extends AppCompatActivity implements View.OnClickL
         }
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case SEND_SMS_FROM_SELECTED_SIM_CARD:
+                if (grantResults.length > 0) {
+                    if (grantResults[0] == PackageManager.PERMISSION_GRANTED
+                            && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                        //Start Custom Gallery Activity by passing intent id
+                        Intent gridActivityIntent = new Intent(LauncherActivity.this, GalleryActivity.class);
+                        gridActivityIntent.putExtra(KEY_IMAGE_DATA_FROM_LAUNCHER_SCREEN, selectedImagesToSendOnGridActivity);//Convert Array into string to pass data
+                        startActivityForResult(gridActivityIntent, CustomGallerySelectId);
+                    } else if (grantResults[0] == PackageManager.PERMISSION_DENIED ||
+                            grantResults[1] == PackageManager.PERMISSION_DENIED) {
+                        if (ActivityCompat.shouldShowRequestPermissionRationale(LauncherActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    buildAlertMessageForPermission();
+                                }
+                            });
+                        } else {
+                            showForcefullPermissionDialog();
+                        }
+                    }
+                }
+                break;
+        }
+    }
+
+    public void showForcefullPermissionDialog() {
+        final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle("Permissions Required").setMessage("You have forcefully denied some of the required permissions " +
+                "for this action. Please open settings, go to permissions and allow them.")
+                .setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.fromParts("package", getPackageName(), null));
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .setCancelable(false)
+                .create()
+                .show();
+    }
+
     //Load GridView
     private void loadGridView(ArrayList<ImgDetailDO> imageDetailData) {
-        GridAdapter adapter = new GridAdapter(LauncherActivity.this, imageDetailData, false,new SparseBooleanArray());
+        GridAdapter adapter = new GridAdapter(LauncherActivity.this, imageDetailData, false, new SparseBooleanArray());
         selectedImageGridView.setAdapter(adapter);
+    }
+
+    public void buildAlertMessageForPermission() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(LauncherActivity.this);
+        builder.setMessage("Gallery app requires read STORAGE permissions.\nWithout which you can not proceed.");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                dialog.cancel();
+                ActivityCompat.requestPermissions(LauncherActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, SEND_SMS_FROM_SELECTED_SIM_CARD);
+//                Intent gpsIntent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+//                startActivityForResult(gpsIntent, 10009);
+
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+
+            }
+        });
+        AlertDialog alert = builder.create();
+        if (!isFinishing())
+            alert.show();
+
     }
 }
